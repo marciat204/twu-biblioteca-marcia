53dc1887a51ec71f770ef56c36b4c72f43bfabe7
Marcia Cavalcante <marcia.cavalcante7@gmail.com>
2018-03-09 10:09:07 -0300

    Refactoring BookManager to Generate Book Mock

--- src/com/twu/book/BookManager.java
+++ src/com/twu/book/BookManager.java
@@ -6,12 +6,17 @@ import java.util.List;
 public class BookManager {
 
     public List<Book> getAll() {
-        List<Book> allBooks = new ArrayList<Book>();
+        List<Book> allBooks = generateBooksMock();
+        return allBooks;
+    }
+
+    private List<Book> generateBooksMock(){
+        List<Book> allBooksMock = new ArrayList<Book>();
         Book book1 = new Book("Book1", 2009, "Author1");
         Book book2 = new Book("Book2", 2099, "Author2");
 
-        allBooks.add(book1);
-        allBooks.add(book2);
-        return allBooks;
+        allBooksMock.add(book1);
+        allBooksMock.add(book2);
+        return allBooksMock;
     }
 }
