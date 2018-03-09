616183d7c67fd758a89b9909123f1c22eec34a0a
Marcia Cavalcante <marcia.cavalcante7@gmail.com>
2018-03-09 13:34:16 -0300

    User can Choose one option at main menu

--- src/menu/MenuOptions.java
+++ src/menu/MenuOptions.java
@@ -1,7 +1,19 @@
 package menu;
 
+import com.twu.book.BookManager;
+
 public class MenuOptions {
+    private BookManager bookManager;
+
+    public MenuOptions() {
+        bookManager = new BookManager();
+    }
+
     public String getMainMenuList() {
         return "1. List Books";
+    }
+
+    public String chooseOption(int option) {
+        return bookManager.getAllBooksDetailsList();
     }
 }
--- test/menu/MenuOptionsTest.java
+++ test/menu/MenuOptionsTest.java
@@ -1,18 +1,36 @@
 package menu;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
 public class MenuOptionsTest {
 
+    MenuOptions menuOptions;
+
+    @Before
+    public void setUp(){
+        menuOptions = new MenuOptions();
+    }
+
     @Test
     public void ShouldShowMainMenuOptions(){
         String expectedMainMenuOptions = "1. List Books";
-        MenuOptions menuOptions = new MenuOptions();
-
         String actualMainMenuOptions = menuOptions.getMainMenuList();
 
         assertEquals(expectedMainMenuOptions, actualMainMenuOptions);
+    }
+
+    @Test
+    public void ShouldChooseListBookOptionFromMainMenu(){
+        String expectedBookList = "Name\t\tAuthor\t\tYear\n\n"+
+                "Book1\t\tAuthor1\t\t2009\n"+
+                "Book2\t\tAuthor2\t\t2009\n"+
+                "Book3\t\tAuthor3\t\t2019";
+
+        String actualBookList = menuOptions.chooseOption(1);
+
+        assertEquals(expectedBookList, actualBookList);
     }
 }
